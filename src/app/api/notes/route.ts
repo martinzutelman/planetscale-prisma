105550ef9e91ef4a3ff46b1d1f79162714ad5e24
martinzutelman <zutelmanmartin@gmail.com>
2023-08-07 22:43:55 -0300

    modified get and post to be functional

--- src/app/api/notes/route.ts
+++ src/app/api/notes/route.ts
@@ -1,14 +1,41 @@
 import { NextResponse } from 'next/server';
 import {prisma} from '@/libs/prisma'
 export async function GET(){
-    const notes = await prisma.note.findMany()
-    console.log(notes)
-    return NextResponse.json({
-        message: 'getting notes'
-    });
+    try {
+        const notes = await prisma.note.findMany()
+        return NextResponse.json(notes);
+    } catch (error) {
+        if (error instanceof Error){
+            return NextResponse.json(
+                {
+                    message: error.message,
+                },
+                {
+                    status:500,
+                }
+            );
+        }
+    }
 } 
-export function POST(){
-    return NextResponse.json({
-        message: 'posting notes'
+export async function POST(request : Request){
+    try {
+          const {title, content} = await request.json();
+            const newNote = await prisma.note.create({
+            data: { 
+            title,
+            content 
+        } 
     })
+    return NextResponse.json(newNote)
+    } catch (error) {
+        if (error instanceof Error){
+            return NextResponse.json(
+                {message: error.message},
+                {status:500}
+            )
+        }
+        return NextResponse.json(error,{status:500})
+    }
+  
 }
+ 
