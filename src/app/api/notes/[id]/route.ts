8a63e38e3d97f3c7c5dc9a6ed714957737c81d9d
martinzutelman <zutelmanmartin@gmail.com>
2023-08-08 15:07:01 -0300

    added getting and deleting notes with custom error

--- src/app/api/notes/[id]/route.ts
+++ src/app/api/notes/[id]/route.ts
@@ -1,15 +1,67 @@
 import { NextResponse } from 'next/server';
-export function GET(){
-    return NextResponse.json({
-        message: 'getting single note...'
-    });
+import { Prisma } from '@prisma/client';
+import {prisma} from '@/libs/prisma'
+interface Params{
+    params: {id: string};
 } 
-export function DELETE(){
-    return NextResponse.json({
-        message: 'deleting single note...'
-    });
+export async function GET(_request: Request, {params}: Params){
+    try {
+        const note  = await prisma.note.findFirst({
+            where: 
+            {
+                id: Number(params.id)
+            },
+        })
+        if (!note) return NextResponse.json({message: 'Note not found'}, {status: 404});
+        return NextResponse.json(note);
+    } catch (error) {
+            if (error instanceof Error){
+                return NextResponse.json(
+                    {
+                        message: error.message,
+                    },
+                    {
+                        status:500,
+                    }
+                )
+            }
+    }
+} 
+export async function DELETE(_request: Request, {params}: Params){
+    try {
+        const deletedNote  = await prisma.note.delete({
+            where: 
+            {
+                id: Number(params.id)
+            },
+        })
+        if (!deletedNote) return NextResponse.json({message: 'Note not found'}, {status: 404});
+        return NextResponse.json(deletedNote);
+    } catch (error) {
+        if (error instanceof Prisma.PrismaClientKnownRequestError){
+            if (error.code === 'P2025'){
+                return NextResponse.json(
+                    {
+                        message: "note not found",
+                    },
+                    {
+                        status: 404,
+                    }
+                )
+            }
+            return NextResponse.json(
+                {
+                    message: error.message,
+                },
+                {
+                    status:500,
+                }
+            )
+        }   
+    }
+   
 }
-export function PUT(){
+export function PUT(request: Request){
     return NextResponse.json({
         message: 'updating single note...'
     });  
--- src/libs/prisma.ts
+++ src/libs/prisma.ts
@@ -4,4 +4,3 @@ declare global{
 }
 export const prisma = global.prisma || new PrismaClient();
 if (process.env.NODE_ENV != 'production') global.prisma = prisma;
-    
